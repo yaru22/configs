81e7c0f8d5b1f57f570f245b57893727f6d93afc
ghpark <ghpark@GHPARK.na.qualcomm.com>
2009-02-03 16:39:29 -0800

    Added hl-line-mode hook to most of the programming modes (e.g.,c++, c, python, javascript, haskell) that I use

--- emacs/modes.el
+++ emacs/modes.el
@@ -1,5 +1,12 @@
 (add-to-list 'load-path "~/.emacs.d/modes/")
 
+;; hl-line-mode to all the programming modes
+(add-hook 'c-mode-hook 'hl-line-mode)
+(add-hook 'c++-mode-hook 'hl-line-mode)
+(add-hook 'javascript-mode-hook 'hl-line-mode)
+(add-hook 'python-mode-hook 'hl-line-mode)
+(add-hook 'haskell-mode-hook 'hl-line-mode)
+
 ;; javascript-mode
 (load "~/.emacs.d/modes/javascript-mode")
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
