f9fd6b87da25e83bf861299eb08b42cd7f32b82c
Brian Park <brian@brian-desktop.(none)>
2010-02-27 00:44:20 -0500

    Removed haskell-mode since it is supported by debian package.

--- emacs/modes.el
+++ emacs/modes.el
@@ -33,11 +33,4 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
 
 ;; haskell-mode
-(load "~/.emacs.d/modes/haskell-mode/haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
-
-;; scala-mode
-(add-to-list 'load-path "~/.emacs.d/modes/scala-mode/")
-(require 'scala-mode-auto)
+(require 'inf-haskell)
