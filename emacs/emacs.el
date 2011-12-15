a05e7c15ef7bd4008be521c9bba23e84b9ea4464
Brian Park <yaru22@gmail.com>
2011-12-14 23:43:23 -0500

    Adding more ELPA package repos to emacs.el

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,3 +1,9 @@
+;; Emacs 24 comes with Emacs Lisp Package Archive (ELPA). The following sets
+;; the list of package repos.
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
 (load "~/.emacs.d/ui")
 (load "~/.emacs.d/keys")
 (load "~/.emacs.d/utils")
