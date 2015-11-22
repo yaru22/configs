7ce9d57fe7a72c2147d1e52ad8ab57c478a0fc72
Brian Park <yaru22@gmail.com>
2015-11-22 15:37:39 -0500

    Emacs: changed package repos.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,19 +1,8 @@
 ;; Emacs 24 comes with Emacs Lisp Package Archive (ELPA). The following sets
 ;; the list of package repos.
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
 (load "~/.emacs.d/ui")
 (load "~/.emacs.d/keys")
