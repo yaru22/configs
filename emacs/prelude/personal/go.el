70f347bcc25158155a923a7eb626147c6b2d457c
Brian Park <yaru22@gmail.com>
2015-11-30 22:48:31 -0500

    Emacs: Edited go.el to expose more GO related env vars.

--- emacs/prelude/personal/go.el
+++ emacs/prelude/personal/go.el
@@ -1,4 +1,9 @@
-;; Go
+(exec-path-from-shell-copy-env "GOPATH")
+(exec-path-from-shell-copy-env "GOBIN")
+(exec-path-from-shell-copy-env "GO15VENDOREXPERIMENT")
+(setq go-projectile-switch-gopath 'never)
+(setq go-projectile-tools-path (getenv "GOBIN"))
+
 (add-hook 'go-mode-hook '(lambda ()
   (local-set-key (kbd "C-c C-f") 'gofmt)))
 (add-hook 'go-mode-hook '(lambda ()
@@ -8,10 +13,9 @@
 (add-hook 'go-mode-hook '(lambda ()
   (local-set-key (kbd "C-c C-k") 'godoc)))
 (defun my-go-mode-hook ()
-  (setq gofmt-command "goimports")
+  (setq gofmt-command "goreturns")
   (add-hook 'before-save-hook 'gofmt-before-save)
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet")))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-
---
+++ emacs/prelude/personal/markdown.el
@@ -0,0 +1,1 @@
+(setq markdown-command "marked")
